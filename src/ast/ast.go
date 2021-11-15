032e2aa087dc523336d289ba5a1aa9729169c29b
allergictoeng <CIANDT\marceloamaral@lnb028228bhz.ciandt.global>
2021-11-15 10:05:12 -0300

    add AST implementaion and test

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -1,12 +1,15 @@
 package ast
 
 import (
+	"bytes"
+
 	"github.com/allergictoeng/Toy-Interpreter/src/token"
 )
 
 type Node interface {
 	// method that returns the literal value of the token it’s associated with. TokenLiteral() will be used only for debugging and testing.
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -36,6 +39,43 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
 type Identifier struct {
 	Token token.Token // the token.IDENT
 	Value string
@@ -47,6 +87,13 @@ type LetStatement struct {
 	Value Expression
 }
 
+type ExpressionStatement struct {
+	Token      token.Token // the first token of the expression
+	Expression Expression
+}
+
+func (i *Identifier) String() string { return i.Value }
+
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
@@ -55,3 +102,6 @@ func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
